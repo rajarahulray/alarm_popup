5dc41933a08d9c8d65e338c911bb0d1afb2476e1
rajarahulray <rajaray2208@gmail.com>
2019-06-27 23:01:10 +0530

    addeed_option_to_set_time_for_sleep

--- alarm_popup_1.0.py
+++ alarm_popup_1.0.py
@@ -82,11 +82,17 @@ def exit_program():
     exit(0)
 
 
-def run_again():
-    os.system('python3 alarm_popup_1.0.py 1 --message="Take_a_break_and_refresh"')
+def run_again(sleep_time):
+    try:
+        os.system('python3 {}/alarm_popup_1.0.py {} --message="Take_a_break_and_refresh"'.format(os.getcwd(),
+                                                                                                 sleep_time))
+    except SystemError:
+        os.system('python {}/alarm_popup_1.0.py {} --message="Take_a_break_and_refresh"'.format(os.getcwd(),
+                                                                                                sleep_button))
 
 
 def sleep_pc():
+    sleep_time = time_setter.get() if time_setter.get() else 60
     if platform.system() == "Windows":
         try:
             os.system('echo "PC going to sleep now.."')
@@ -96,7 +102,7 @@ def sleep_pc():
             cur_time = str(time.localtime()[3]) + ":" + str(time.localtime()[4]) + ":" + str(time.localtime()[5])
             print("Alarm switched off at : {}".format(cur_time))
             exit(0)
-            run_again()
+            run_again(sleep_time)
     # for ubuntu..
     elif platform.system() == "Linux":
         try:
@@ -108,7 +114,7 @@ def sleep_pc():
             print("Alarm switched off at : {}".format(cur_time))
             print('System is going to sleep now...')
             root.destroy()
-            run_again()
+            run_again(sleep_time)
         except KeyboardInterrupt:
             print('Interrupted by user...')
         exit(0)
@@ -125,4 +131,7 @@ alm_lbl.config(bg='white', font=('Calibri', 13))
 alm_lbl.pack()
 sleep_button = tk.Button(root, text="Sleep", command=sleep_pc).pack(side='left', padx=5, pady=5)
 exit_button = tk.Button(root, text="Exit", command=exit_program).pack(side='right', padx=5, pady=5)
+time_setter = tk.Entry(root)
+time_setter.pack()
+time_setter.config(width=10)
 root.mainloop()
--- tests_and_references/alarm_window.py
+++ tests_and_references/alarm_window.py
@@ -4,11 +4,12 @@ import platform
 import pexpect
 
 
-def run_again():
-    os.system('python3 alarm_popup.py 1 --message="Take_a_break_and_refresh"')
+def run_again(sleep_time):
+    os.system('python3 alarm_popup.py {0} --message="Take_a_break_and_refresh"'.format(sleep_time))
 
 
 def sleep_pc():
+    sleep_time = time_setter.get() if time_setter.get() else 60
     if platform.system() == "Windows":
         try:
             os.system('echo "PC going to sleep now.."')
@@ -16,7 +17,7 @@ def sleep_pc():
         except OSError:
             os.system('''%windir%\System32\rundll32.exe powrprof.dll,SetSuspendState Hibernate''')
             exit(0)
-            run_again()
+            run_again(sleep_time)
     # for ubuntu..
     elif platform.system() == "Linux":
         try:
@@ -26,7 +27,7 @@ def sleep_pc():
             print(sleep_child.read())
             print('System is going to sleep now...')
             root.destroy()
-            run_again()
+            run_again(sleep_time)
         except KeyboardInterrupt:
             print('Interrupted by user...')
         exit(0)
@@ -40,4 +41,7 @@ alm_lbl.config(bg='white', font=('Calibri', 13))
 alm_lbl.pack()
 sleep_button = t.Button(root, text="Sleep", command=sleep_pc).pack(side='left', padx=5, pady=5)
 exit_button = t.Button(root, text="Exit", command=exit).pack(side='right', padx=5, pady=5)
+time_setter = t.Entry(root)
+time_setter.pack()
+time_setter.config(width=10)
 root.mainloop()
